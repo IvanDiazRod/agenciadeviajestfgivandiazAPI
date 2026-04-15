a1e99ffa3fb1c6f947976e05566deb12c709ae8b
IvanDiazRod <157109906+IvanDiazRod@users.noreply.github.com>
2026-04-15 15:53:44 +0200

    fix

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TourController;
+use App\Http\Controllers\BookingController;
 
 Route::get('/ping', function () {
     return ['message' => 'pong'];
@@ -14,5 +15,9 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/bookings', [BookingController::class, 'store']);
+    Route::get('/my-bookings', [BookingController::class, 'myBookings']);
+
     Route::post('/user/photo', [AuthController::class, 'updateProfilePhoto']);
 });
